6b1197368b539d79ade0336e58ff1dcdfe57dacc
AsimSafarli <asimseferli014@gmail.com>
2023-05-03 15:09:16 +0400

    Day1

---
+++ Day1/script.js
@@ -0,0 +1,44 @@
+console.log(2**5)// 2 ustu 5 5-in yerine istenilen reqem ola biler.Burda riyazi əməllər aparmaq olar
+/*
+Js-də data typeları  iki cür olur .Primitiv və Primitiv olmayan .İbtidai və ibtidai olmayan.
+1-ci gün sadece primitiv(İbtidai ) data tiplərinə baxacayq
+Number
+String 
+Undefined 
+Null
+Boolen
+*/ 
+
+/**
+  Number tipi -1,2,-3,0 və s
+ */
+/**
+  String tipi eger reqem '5' bele qeyd olunubsa stringdir amma ki 5 bele qeyd olunubsa o zaman numberdi. Cox zaman qatisdirilir bu movzular.
+  String "",'',`` istifade edilir adlar , olkeler ve s string olarak gedir
+ */
+/**
+    Null - bos deyer  demekdir 
+ */
+/*Undefined - belli edilmemis yeni let name  yazb name-i console.log-a yazsaq gorerikki bize undefined donecekdir 
+*/
+//her birine aid misal 
+let example
+console.log(typeof 'Alison')//typeof burda type-nı tapmaq üçün istifade olunur
+console.log(typeof 5)//number
+console.log(typeof null)//null
+console.log(typeof example)//undefined
+
+/*Personal Information*/
+let name ='Alison'
+let sname ='Jony'
+let country='Azerbaijan'
+let age = 20
+let married =false
+console.log(name,sname,age,country,married)
+let a=5
+let b=3
+console.log(a*b)
+console.log(a-b)
+console.log(a+b)
+console.log(a**b)
+console.log(a/b)
